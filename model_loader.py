19bf5c909195dad003b6df2445101b934498e095
Ben Jacobsohn <ben.jacobsohn07@gmail.com>
2024-10-09 11:42:21 -0400

    missed a file xd

--- model_loader.py
+++ model_loader.py
@@ -1,10 +1,9 @@
 import os
 import cifar10.model_loader
-import mnist.basicnet
-
+from mnist import basicnet
 def load(dataset, model_name, model_file, data_parallel=False):
     if dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
     if dataset == 'mnist':
-        net = mnist.basicnet()
+        net = basicnet.BasicNet()
     return net
